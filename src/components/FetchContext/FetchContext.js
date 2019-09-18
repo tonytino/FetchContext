0b9e9ddc372c6acfdfa18a677208ea2d0f5ef2af
Anthony Hernandez <tony91139@gmail.com>
2019-09-18 11:50:44 -0700

    Implement fetchers in <FetchContext />

--- src/components/FetchContext/FetchContext.js
+++ src/components/FetchContext/FetchContext.js
@@ -1,45 +1,106 @@
 import React from 'react';
 import Context from './Context';
+import {
+  fetchCacheFallbackNetworkResponse,
+  fetchCacheThenNetworkResponse,
+  fetchNetworkFallbackCacheResponse,
+  fetchNetworkResponse,
+} from './fetchers';
+
+const CACHING_STRATEGIES = [
+  'cacheFallbackNetwork',
+  'cacheThenNetwork',
+  'network',
+  'networkFallbackCache',
+];
 
 /**
- * @description Performs GET for endpoint provided after page paint, then it
- *  makes the response available via context.
+ * Loads requests received after page-paint, exposing the response in context.
  *
- *  https://reactjs.org/docs/hooks-reference.html#timing-of-effects
+ * **NOTE:** This component will NOT manage your cache lifetime.
  *
+ * Page-paint: https://reactjs.org/docs/hooks-reference.html#timing-of-effects
+ *
+ * @param {string} cacheName - The name to store the cache under
+ * @param {string} cachingStrategy - The strategy to employ for fetching/caching.
+ *  Options include:
+ *  - cacheFallbackNetwork
+ *  - cacheThenNetwork
+ *  - network (default)
+ *  - networkFallbackCache
  * @param defaultData - The initial data to load (i.e. before API response)
- * @param endpoint - The URL to fetch (GET endpoint)
+ * @param request - A Request object or a URL string
  */
 function FetchContext(props) {
+  let { cachingStrategy = 'network' } = props;
   const {
     children = null,
+    cacheName = 'fetchContextCache',
     defaultData = [],
-    endpoint = 'https://jsonplaceholder.typicode.com/todos',
+    request = 'https://jsonplaceholder.typicode.com/todos',
   } = props;
 
+  // Default to 'network' if we received an unsupported caching strategy
+  if (!CACHING_STRATEGIES.includes(cachingStrategy)) {
+    console.warn(
+      `<FetchContext /> received an invalid value for prop 'cachingStrategy'. `+
+      `Received ${cachingStrategy}. Defaulting value to 'network' instead.`
+    )
+    cachingStrategy = 'network';
+  }
+
+  // ['default', 'cache', 'network'] // default is the default data defined
+  const [dataSource, setDataSource] = React.useState('default');
   const [data, setData] = React.useState(defaultData);
 
   const fetchContext = {
+    cachingStrategy,
     data,
+    dataSource,
     defaultData,
-    endpoint,
+    request, // This will be whatever was passed in! So either String or Request
   };
 
   React.useEffect(() => {
-    async function performFetch(endpoint) {
-      await fetch(endpoint)
-        .then(response => response.json())
-        .then(json => setData(json));
+    switch (cachingStrategy) {
+      case 'cacheFallbackNetwork':
+        fetchCacheFallbackNetworkResponse(
+          request,
+          cacheName,
+          setData,
+          setDataSource
+        );
+        break;
+      case 'cacheThenNetwork':
+        fetchCacheThenNetworkResponse(
+          request,
+          cacheName,
+          setData,
+          setDataSource
+        );
+        break;
+      case 'network':
+        fetchNetworkResponse(request, setData, setDataSource);
+        break;
+      case 'networkFallbackCache':
+        fetchNetworkFallbackCacheResponse(
+          request,
+          cacheName,
+          setData,
+          setDataSource
+        );
+        break;
+      default:
+        fetchNetworkResponse(request, setData, setDataSource);
+        break;
     }
-
-    performFetch(endpoint)
-  }, [endpoint]);
+  }, [cacheName, cachingStrategy, request]);
 
   return (
     <Context.Provider value={fetchContext}>
       {children}
     </Context.Provider>
-  )
+  );
 }
 
 export default FetchContext;
